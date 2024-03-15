f50ef872227cf5b02f15f31be64e877afac76f92
Renat871 <rena2007at@gmail.com>
2024-03-15 20:21:04 +0100

    second commit

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
